d8854125a0afe11113d83eb38a705418ea79bce5
James Bajamundi <jamesbjmnd@gmail.com>
2019-12-14 10:52:52 -0800

    basic serverside code

--- server.js
+++ server.js
@@ -1,1 +1,44 @@
-var express = require("express");
+var express = require('express');
+var path = require('path');
+
+var app = express();
+var PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, function () {
+    console.log('App listening on PORT: ' + PORT)
+});
+
+// DATA
+var tableArray = [
+    {
+        customerName: '',
+        customerEmail: '',
+        customerID: '',
+        phoneNumber: ''
+    }
+];
+
+var waitinglistArray = [
+    {
+        customerName: '',
+        customerEmail: '',
+        customerID: '',
+        phoneNumber: ''
+    }
+]
+
+//ROUTES
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'home.html'))
+});
+
+app.get('/api/tables', function (req, res) {
+    res.json();
+});
+
+app.get('/api/waitingList', function (req, res) {
+    res.json();
+});
